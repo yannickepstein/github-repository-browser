c84280a105e134c33d45c875cbf34aedabc4494b
Yannick Epstein <yannick.epstein@gmail.com>
2020-08-11 21:08:34 +0200

    Github Authentication Middleware
    Setup some middleware to access the gihub api with a auth token using the graphql module.

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,23 +1,36 @@
-import {NgModule} from '@angular/core';
-import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
-import {HttpLink} from 'apollo-angular/http';
+import { NgModule } from '@angular/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { InMemoryCache } from '@apollo/client/core';
+import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
+import { ApolloLink, concat } from 'apollo-link';
+import { HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
-const uri = 'https://api.github.com/graphql'; // <-- add the URL of the GraphQL server here
-export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
-  return {
-    link: httpLink.create({uri}),
-    cache: new InMemoryCache(),
-  };
-}
+const uri = 'https://api.github.com/graphql';
+const token = '';
+
+const authTokenMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
+  });
+  return forward(operation);
+});
 
 @NgModule({
-  providers: [
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: createApollo,
-      deps: [HttpLink],
-    },
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    HttpLinkModule
   ],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory: (httpLink: HttpLink) => {
+      return {
+        cache: new InMemoryCache(),
+        link: authTokenMiddleware.concat(httpLink.create({uri}))
+      }
+    },
+    deps: [HttpLink]
+  }],
 })
 export class GraphQLModule {}
